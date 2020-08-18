1f3a13c9eb2622f235c8c1c9ed842073094ffb34
Perez Mac <apple@Timring.local>
2020-08-18 01:37:33 +0100

    add TabAdapter class for adapter

--- app/src/main/java/com/example/tabview/MainActivity.kt
+++ app/src/main/java/com/example/tabview/MainActivity.kt
@@ -11,57 +11,32 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    var tabs: TabLayout? = null
-    var view: ViewPager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tabs = findViewById<TabLayout>(R.id.tbTabs)
-        var view = findViewById<ViewPager>(R.id.vpView)
+        val tabs = findViewById<TabLayout>(R.id.tlTabs)
+        val view = findViewById<ViewPager>(R.id.vpView)
 
-        tabs!!.addTab(tabs!!.newTab().setText("Home"))
-        tabs!!.addTab(tabs!!.newTab().setText("Favourites"))
-        tabs!!.addTab(tabs!!.newTab().setText("Search"))
-        tabs!!.tabGravity = TabLayout.GRAVITY_FILL
+        //  FILL TAB WITH ITEMS
+        tabs.addTab(tabs.newTab().setText("Home"))
+        tabs.addTab(tabs.newTab().setText("Favourites"))
+        tabs.addTab(tabs.newTab().setText("Search"))
+        tabs.tabGravity = TabLayout.GRAVITY_FILL
 
-        val adapter = TabAdapter(this, supportFragmentManager, tabs!!.tabCount)
-        view!!.adapter = adapter
+        //GET THE ADAPTER
+        val adapter = TabAdapter(this, supportFragmentManager, tabs.tabCount)
+        view.adapter = adapter
 
-        view!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
-
-        tabs!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        //
+        view.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                view!!.currentItem = tab.position
+                view.currentItem = tab.position
             }
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
-
-
-
-
-//        var tabView = findViewById<TabLayout.TabView>(R.id.tabLayout)
-//        var homeTab = findViewById<TabItem>(R.id.homeTab)
-//        var favouritesTab = findViewById<TabItem>(R.id.favoritesTab)
-//        var searchTab = findViewById<TabItem>(R.id.searchTab)
-//        var viewTab = findViewById<ViewPager>(R.id.viewPager)
-//
-//        val tabAdapter: TabAdapter = TabAdapter(this, supportFragmentManager, tabView.childCount)
-//        viewTab.adapter = tabAdapter
-
-
-
-
-
-//        fun resetColor(){
-//            //RESET ALL BUTTONS COLOR TO DECAGON GREEN
-//            DrawableCompat.setTint(homeTab.icon, ContextCompat.getColor(this, R.color.decagonGreen))
-//            DrawableCompat.setTint(favouritesTab.icon, ContextCompat.getColor(this, R.color.decagonGreen))
-//            DrawableCompat.setTint(searchTab.icon, ContextCompat.getColor(this, R.color.decagonGreen))
-//        }
-//        resetColor()
-
     }
 }
--- app/src/main/java/com/example/tabview/TabAdapter.kt
+++ app/src/main/java/com/example/tabview/TabAdapter.kt
@@ -7,5 +7,15 @@ import androidx.fragment.app.FragmentPagerAdapter
 
 class TabAdapter(private val cntx: Context, fm: FragmentManager, internal var allTabs: Int): FragmentPagerAdapter(fm) {
 
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0 -> { return HomeFragment() }
+            1 -> { return FavouritesFragment() }
+            else -> { return SearchFragment() }
+        }
+    }
 
+    override fun getCount(): Int {
+        return allTabs
+    }
 }
